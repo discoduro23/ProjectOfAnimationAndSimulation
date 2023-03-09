c9bb3e53d7103e71db9c2bb444ba2dade4eec505
Ger1y1 <63232421+Ger1y1@users.noreply.github.com>
2023-03-09 20:44:09 +0100

    mas gusano con cola

--- Assets/Scripts/NewBodySpawn.cs
+++ Assets/Scripts/NewBodySpawn.cs
@@ -6,6 +6,7 @@ public class NewBodySpawn : MonoBehaviour
 {
     public int bodyparts = 0;
     public GameObject bodyPrefab;
+    public GameObject tailPrefab;
     public GameObject head;
     public GameObject[] body;
     
@@ -17,22 +18,37 @@ public class NewBodySpawn : MonoBehaviour
         {
             if (i > 0)
             {
-                body[i] = Instantiate(bodyPrefab, body[i - 1].transform.position, Quaternion.identity);
-                body[i].transform.parent = transform;
-                body[i].transform.position = body[i - 1].transform.position + new Vector3(0, 0, 1);
-                body[i - 1].GetComponent<CharacterJoint>().connectedBody = body[i].GetComponent<Rigidbody>();
+                if (i == bodyparts - 1)
+                {
+                    body[i] = Instantiate(tailPrefab, body[i - 1].transform.position, body[i - 1].transform.rotation);
+                    body[i].transform.parent = transform;
+                    body[i].transform.position = body[i - 1].transform.position + new Vector3(-1, 0, 0);
+                    body[i - 1].GetComponent<CharacterJoint>().connectedBody = body[i].GetComponent<Rigidbody>();
+                }
+                else
+                {
+                    body[i] = Instantiate(bodyPrefab, body[i - 1].transform.position, body[i - 1].transform.rotation);
+                    body[i].transform.parent = transform;
+                    body[i].transform.position = body[i - 1].transform.position + new Vector3(-1, 0, 0);
+                    body[i - 1].GetComponent<CharacterJoint>().connectedBody = body[i].GetComponent<Rigidbody>();
+                }
+                
+                
             }
             else
             {
-                body[i] = Instantiate(bodyPrefab, head.transform.position, Quaternion.identity);
+                body[i] = Instantiate(bodyPrefab, head.transform.position, head.transform.rotation);
                 body[i].transform.parent = transform;
-                body[i].transform.position = head.transform.position + new Vector3(0, 0, 1);
+                body[i].transform.position = head.transform.position + new Vector3(-1, 0, 0);
                 head.GetComponent<CharacterJoint>().connectedBody = body[i].GetComponent<Rigidbody>();
             }
+            
 
             if (i == bodyparts - 1)
             {
-                body[i].GetComponent<CharacterJoint>().connectedBody = null;
+                //errase component characterjoint from last bodypart
+                Destroy(body[i].GetComponent<CharacterJoint>());
+
             }
 
         }

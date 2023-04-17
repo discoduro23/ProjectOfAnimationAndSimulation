9611d43b7780bcdc36d39271997f01c5b09b2e6c
Ger1y1 <jermanjer17@gmail.com>
2023-04-17 15:54:18 +0200

    balas
    Y pum
    Yastaqui la guerra

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -16,10 +16,25 @@ public class TurretController : MonoBehaviour
     public GameObject Turret2;
     public GameObject TurretBarrel;
 
+    public GameObject bullet;
+    public GameObject bulletsParent;
+    public GameObject bulletSpawn;
+    public float bulletSpeed = 1000f;
+    public int bulletCount = 10;
+    private GameObject[] bullets;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        bullets = new GameObject[bulletCount];
+        for (int i = 0; i < bulletCount; i++)
+        {
+            GameObject bulletClone = Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bulletClone.transform.parent = bulletsParent.transform;
+            bulletClone.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.up * bulletSpeed);
+            bulletClone.SetActive(false);
+            bullets[i] = bulletClone;
+        }
     }
 
     // Update is called once per frame
@@ -57,5 +72,20 @@ public class TurretController : MonoBehaviour
         Turret.transform.Rotate(0, currentHSpeed, 0, Space.Self);
         Turret2.transform.Rotate(currentVSpeed, 0, 0, Space.Self);
 
+
+        //shoot the bullet
+        if (Input.GetButtonDown("Fire1"))
+        {
+            for (int i = 0; i < bulletCount; i++)
+            {
+                if (!bullets[i].activeInHierarchy)
+                {
+                    bullets[i].SetActive(true);
+                    bullets[i].transform.position = bulletSpawn.transform.position;
+                    bullets[i].GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.up * bulletSpeed);
+                    break;
+                }
+            }
+        }
     }
 }

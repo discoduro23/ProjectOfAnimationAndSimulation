ef2dada86285efdb5ae00717fa2519e6537f9e45
Daniel Sanchez <alu.115081@usj.es>
2023-05-30 13:49:34 +0200

    luego nos vemos

---
+++ Assets/Scripts/Bullet-Turret/TurretController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public float maxSpeedH = 10.0f;
+    public float accelH = 2f;
+    public float maxSpeedV = 10.0f;
+    public float accelV = 2f;
+    public float forceBreak = 2f;
+    public float currentHSpeed;
+    public float currentVSpeed;
+    public GameObject TurretBase;
+    public GameObject Turret;
+    public GameObject Turret2;
+    public GameObject TurretBarrel;
+
+    public GameObject bullet;
+    public GameObject bulletsParent;
+    public GameObject bulletSpawn;
+    public float bulletSpeed = 1000f;
+    public int bulletCount = 10;
+    private GameObject[] bullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullets = new GameObject[bulletCount];
+        for (int i = 0; i < bulletCount; i++)
+        {
+            GameObject bulletClone = Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bulletClone.transform.parent = bulletsParent.transform;
+            bulletClone.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.up * bulletSpeed);
+            bulletClone.SetActive(false);
+            bullets[i] = bulletClone;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //control the turret with the inputs with acceleration
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            currentHSpeed = Mathf.MoveTowards(currentHSpeed, maxSpeedH, accelH * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            currentHSpeed = Mathf.MoveTowards(currentHSpeed, -maxSpeedH, accelH * Time.deltaTime);
+        }
+        else
+        {
+            currentHSpeed = Mathf.MoveTowards(currentHSpeed, 0,forceBreak * accelH * Time.deltaTime);
+        }
+
+        if(Turret2.transform.rotation.x < 30 && Turret2.transform.rotation.x > -10)
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            currentVSpeed = Mathf.MoveTowards(currentVSpeed, maxSpeedV, accelV * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            currentVSpeed = Mathf.MoveTowards(currentVSpeed, -maxSpeedV, accelV * Time.deltaTime);
+        }
+        else
+        {
+            currentVSpeed = Mathf.MoveTowards(currentVSpeed, 0, forceBreak * accelV * Time.deltaTime);
+        }
+
+        Turret.transform.Rotate(0, currentHSpeed, 0, Space.Self);
+        Turret2.transform.Rotate(currentVSpeed, 0, 0, Space.Self);
+
+
+        //shoot the bullet
+        if (Input.GetButtonDown("Fire1"))
+        {
+            for (int i = 0; i < bulletCount; i++)
+            {
+                if (!bullets[i].activeInHierarchy)
+                {
+                    bullets[i].SetActive(true);
+                    bullets[i].transform.position = bulletSpawn.transform.position;
+                    bullets[i].GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.up * bulletSpeed, ForceMode.Acceleration);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnThings/SpawnManagerControler.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerControler : MonoBehaviour
+{
+
+    //The prefab to spawn
+    [SerializeField] GameObject prefab = null;
+
+    //The number of prefabs to spawn
+    [SerializeField] int numberOfPrefabs = 4;
+
+    //The dimensions of the cube where the prefabs will be spawned
+    [SerializeField] Vector3 cubeDimensions = Vector3.one;
+
+    // Make the dimensions a little smaller so the prefabs don't spawn on the edges
+    [SerializeField] float offset = 10f;
+
+    //When spawning an object it's needed to throw a raycast to the -y direction to know where to spawn it, getting the normals of that point and spawning acordingly
+    [SerializeField] float raycastDistance = 50f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            SpawnPrefabs();
+        }
+        
+    }
+
+    private void SpawnPrefabs()
+    {
+        Random.InitState((int)System.DateTime.Now.Ticks);
+        //Spawn the prefabs
+        for (int i = 0; i < numberOfPrefabs; i++)
+        {
+
+            //Get a random position inside the cube
+            Vector3 randomPosition = new Vector3(
+                Random.Range(0 + offset, cubeDimensions.x - offset),
+                10,
+                Random.Range(0 + offset, cubeDimensions.z - offset));
+
+            Debug.Log("Spawning prefab " + i + " at position " + randomPosition);
+
+            //Throw a raycast to the -y direction to know where to spawn the prefab
+            RaycastHit hit;
+            if (Physics.Raycast(randomPosition, Vector3.down, out hit, raycastDistance))
+            {
+                Debug.Log("Raycast hit at " + hit.point + " with normal " + hit.normal + "grounded? " + hit.collider.gameObject.tag);
+                
+                    //Instantiate the prefab
+                    GameObject go = Instantiate(prefab, hit.point, Quaternion.identity);
+
+                    //Set the rotation of the prefab to the normal of the point where it was spawned
+                    go.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                
+            }
+            else
+            {
+                //Try again
+                i--;
+            }
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, cubeDimensions - new Vector3(offset, 0, offset));
+    }
+    
+
+}
